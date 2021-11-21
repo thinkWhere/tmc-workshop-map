2e13d73d001a23b10382eabf54a6bf3acd8bc855
DafyddLlyr <dafyddllyr@gmail.com>
2021-11-21 21:22:22 +0000

    Setup interactions branch

--- main.js
+++ main.js
@@ -8,12 +8,11 @@ import getWMSLayer from "./wms";
 import initPopover from "./popover";
 import getWFSLayer from "./wfs";
 import initInteractions from "./interactions";
-import initGazetteer from "./gazetteer";
 
 // Create layer from imported functions
 const mastermapWMTS = await getWMTSLayer("os_licensed_background_colour");
 const woodlandWMS = getWMSLayer("sf_nwss");
-const scenicAreasWFS = getWFSLayer("osmm:osmm_topographicarea")
+const mastermapWFS = getWFSLayer("osmm:osmm_topographicarea")
 
 // Set up a new Tile Layer
 const openStreetMap = new TileLayer({
@@ -28,8 +27,8 @@ const map = new Map({
   layers: [
     // openStreetMap,
     mastermapWMTS,
-    woodlandWMS,
-    // scenicAreasWFS,
+    // woodlandWMS,
+    mastermapWFS,
   ],
   // A View object represents a simple 2D view of the map.
   // This is the object to act upon to change the center, resolution, and rotation of the map
@@ -41,15 +40,5 @@ const map = new Map({
   }),
 });
 
-// Zoom to
-function zoomTo(coord_string) {
-  var split = coord_string.split(",");
-  if(split.length == 2) {
-    map.getView().setResolution(0.5);
-    map.getView().setCenter([parseInt(split[0]), parseInt(split[1])]);
-  }
-}
-
-initPopover(map, woodlandWMS);
-// initInteractions(map);
-// initGazetteer(zoomTo);
+// initPopover(map, woodlandWMS);
+initInteractions(map);
